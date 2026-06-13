35513801849198be38fcb71c13c7ded451d516af
Chris Dare <engineering@chrisdare.me>
2026-06-13 18:33:26 +0000

    chore(release): pontifex-mcp 0.3.0 (#55)

--- core/pontifex_mcp/__init__.py
+++ core/pontifex_mcp/__init__.py
@@ -29,7 +29,7 @@ from pontifex_mcp.tool_runtime import InvalidInput, tool_runtime
 from pontifex_mcp.utils.circuit_breaker import CircuitBreaker
 from pontifex_mcp.utils.retry import async_retry
 
-__version__ = "0.2.0"
+__version__ = "0.3.0"
 
 __all__ = [
     "AuditRecord",
